0d1c88de6f4ef156c5db101c6bfeb38ce90d082e
JaeWon Jo <joshjo0108@gmail.com>
2022-01-27 23:34:27 +0800

    SETTING UP Django + UPDATE ON .gitignore

---
+++ test.py
@@ -0,0 +1,6 @@
+# HOW TO SET UP DJANGO IN pipenv
+    # pipenv install django
+    # pipenv shell  -> starting the pipenv
+    # django-admin startproject til .
+    # python3 manage.py migrate -> WILL CREATE db.sqlite3 FILE
+    # python3 manage.py runserver 0.0.0.0:8000
