1f175fb2202a590f20b60f8cfd8031c989e6c6c0
JaeWon Jo <joshjo0108@gmail.com>
2022-01-28 16:12:26 +0800

    SETTING UP DJANGO app

--- test.py
+++ test.py
@@ -4,3 +4,5 @@
     # django-admin startproject til .
     # python3 manage.py migrate -> WILL CREATE db.sqlite3 FILE
     # python3 manage.py runserver 0.0.0.0:8000
+    # python3 manage.py createsuperuser     -> MAKING ID AND PASSWORD
+    # python3 manage.py startapp feed -> NOT START PROJECT, CREATING A PROJECT NAMED 'feed'
