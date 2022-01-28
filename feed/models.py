e3f6175ca495084f349c974ccf607914d04ed897
JaeWon Jo <joshjo0108@gmail.com>
2022-01-28 16:21:23 +0800

    USE OF makemigrations & migrate

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    # THIS VARIABLES ARE MANAGED IN "migrations" FILES
+    # VARIABLES: "1. id", "2. text"
+    text = models.CharField(max_length=240)
--- test.py
+++ test.py
@@ -6,3 +6,5 @@
     # python3 manage.py runserver 0.0.0.0:8000
     # python3 manage.py createsuperuser     -> MAKING ID AND PASSWORD
     # python3 manage.py startapp feed -> NOT START PROJECT, CREATING A PROJECT NAMED 'feed'
+    # python3 manage.py makemigrations -> making "feed/migrations" files WHEN YOU USE models.py
+    # python3 manage.py migrate -> execute the above file
